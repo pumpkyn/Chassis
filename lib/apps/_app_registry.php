576c023dfe0aaedd38986e8bcdb97d653ec97449
Jan Stanik <jan.stanik@creat.io>
2011-05-24 16:20:37 +0100

    New _app_registry interface for embedding <style> element into page header.

--- lib/apps/_app_registry.php
+++ lib/apps/_app_registry.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once CHASSIS_LIB . 'ui/_smarty_wrapper.php';
+
 /**
  * @file _app_registry.php
  * @author giorno
@@ -11,9 +13,6 @@
  * all applications instances, so it can be used to call applications from
  * other applications.
  */
-
-require_once CHASSIS_LIB . 'ui/_smarty_wrapper.php';
-
 class _app_registry
 {
 	/**
@@ -28,21 +27,21 @@ class _app_registry
 
 	/**
 	 * Singleton pattern instance.
-	 * @var <_app_registry> 
+	 * @var _app_registry
 	 */
 	private static $instance = NULL;
 
 	/**
 	 * Associative array of applications instances.
 	 *
-	 * @var <array>
+	 * @var array
 	 */
 	private $apps = NULL;
 
 	/**
 	 * Id of application used as default, fallback, application.
 	 * 
-	 * @var <string>
+	 * @var string
 	 */
 	private $default = NULL;
 
@@ -50,21 +49,21 @@ class _app_registry
 	 * Id of executed application. There should be only one exec in object
 	 * lifetime.
 	 *
-	 * @var <string>
+	 * @var string
 	 */
 	private $executed = NULL;
 
 	/**
 	 * Array containing paths to Javascript libraries required by applications.
 	 *
-	 * @var <array>
+	 * @var array
 	 */
 	protected $jsLibs = NULL;
 
 	/**
 	 * Array containing paths to CSS stylesheets required by applications.
 	 *
-	 * @var <array>
+	 * @var array
 	 */
 	protected $cssLinks = NULL;
 
@@ -72,16 +71,23 @@ class _app_registry
 	 * Javascript codes to be executed in <head> element. This is facility to
 	 * pass initialization and configuration of certain objects.
 	 *
-	 * @var <array>
+	 * @var array
 	 */
 	protected $jsPlain = NULL;
+
+	/**
+	 * CSS fragments to be defined in <head> element.
+	 *
+	 * @var array
+	 */
+	protected $cssPlain = NULL;
 
 	/**
 	 * Array of Javascript codes to be executed in the <body> onLoad event. This
 	 * member use different structure than $jsLibs and $cssLinks. Keys are
 	 * irrelevant here. Only values matter.
 	 *
-	 * @var <array>
+	 * @var array
 	 */
 	protected $onLoad = NULL;
 
@@ -90,7 +96,7 @@ class _app_registry
 	 * <body> elements (as its childs). Indented to be used for SkyDome feature,
 	 * etc.
 	 *
-	 * @var <array> 
+	 * @var array
 	 */
 	protected $bodyChildren = NULL;
 
@@ -117,7 +123,7 @@ class _app_registry
 	 * Registers new application into the registry and informs it about this
 	 * status change.
 	 *
-	 * @param <App> $app application instance
+	 * @param App $app application instance
 	 */
 	public function register( &$app )
 	{
@@ -132,8 +138,8 @@ class _app_registry
 	 * Register new CSS file withing the registry. If path to the resource does
 	 * not exist in internal array, it will be added.
 	 *
-	 * @param <string> $path path to the resource
-	 * @param <string> $by application identifier, used for comments
+	 * @param string $path path to the resource
+	 * @param string $by application identifier, used for comments
 	 */
 	public function requireCss ( $path, $by )
 	{
@@ -142,11 +148,24 @@ class _app_registry
 	}
 
 	/**
+	 * Register new CSS code to be declared in page <head> element. Code is
+	 * required to be passed by reference, so it can be updated till it is
+	 * needed for rendering.
+	 *
+	 * @param string $code CSS code
+	 */
+	public function requireCssPlain ( $code)
+	{
+		if ( !is_array( $this->cssPlain ) || !in_array( $code, $this->cssPlain ) )
+			$this->cssPlain[] = $code;
+	}
+
+	/**
 	 * Register new Javascript library withing the registry. If path to the
 	 * resource does not exist in internal array, it will be added.
 	 *
-	 * @param <string> $path path to the resource
-	 * @param <string> $by application identifier, used for comments
+	 * @param string $path path to the resource
+	 * @param string $by application identifier, used for comments
 	 */
 	public function requireJs ( $path, $by )
 	{
@@ -155,11 +174,11 @@ class _app_registry
 	}
 
 	/**
-	 * Register new Javascript code toe be executed in page <head> element.
-	 * Code is required to be passed by reference, so it can be updated till
-	 * it is needed for rendering.
+	 * Register new Javascript code to be executed in page <head> element. Code
+	 * is required to be passed by reference, so it can be updated till it is
+	 * needed for rendering.
 	 *
-	 * @param <string> $code Javascript code
+	 * @param string $code Javascript code
 	 */
 	public function requireJsPlain ( $code)
 	{
@@ -171,7 +190,7 @@ class _app_registry
 	 * Register new Javascript code toe be executed in <body> element onLoad
 	 * event. Logic prevents double insertion of same code.
 	 *
-	 * @param <string> $code Javascript code
+	 * @param string $code Javascript code
 	 */
 	public function requireOnLoad ( $code)
 	{
@@ -183,8 +202,8 @@ class _app_registry
 	 * Puts Smarty rendering requirements into internal array. Stored path is
 	 * used for including templates directly into <body> element.
 	 *
-	 * @param <string> $path path to Smarty template
-	 * @param <string> $by identifier of software block requiring the resource
+	 * @param string $path path to Smarty template
+	 * @param string $by identifier of software block requiring the resource
 	 */
 	public function requireBodyChild ( $path, $by )
 	{
@@ -196,16 +215,16 @@ class _app_registry
 	 * Returns whether application with given id exists within the repository
 	 * or not, i.e. it is registered or not.
 	 * 
-	 * @param <string> $id application id
-	 * @return <bool> tells if app is registered or not
+	 * @param string $id application id
+	 * @return bool tells if app is registered or not
 	 */
 	public function exists ( $id ) { return ( ( is_array( $this->apps) ) && ( array_key_exists( $id, $this->apps ) ) ); }
 
 	/**
 	 * Executes exec() method on application given by its id.
 	 *
-	 * @param <string> $id application id
-	 * @return <bool> success, false if call fails
+	 * @param string $id application id
+	 * @return bool success, false if call fails
 	 */
 	public function exec( $id )
 	{
@@ -230,8 +249,8 @@ class _app_registry
 	 * application. This can be performed only once, first call of this method
 	 * prevents future changes.
 	 *
-	 * @param <string> $id application id
-	 * @return <bool> success
+	 * @param string $id application id
+	 * @return bool success
 	 */
 	public function setDefault ( $id )
 	{
@@ -248,7 +267,7 @@ class _app_registry
 	 * Returns value of executed application. It can be used to mark icon of app
 	 * as an active one.
 	 *
-	 * @return <string> id of executed app
+	 * @return string id of executed app
 	 */
 	public function getExecutedId ( )
 	{
@@ -258,14 +277,14 @@ class _app_registry
 	/**
 	 * Returns Javascript libraries/scripts.
 	 *
-	 * @return <array> reference to array of registered libraries
+	 * @return array reference to array of registered libraries
 	 */
 	public function getJs ( ) { return $this->jsLibs; }
 
 	/**
 	 * Returns Javascript codes to be planted into <head> element.
 	 *
-	 * @return <array> reference to array of registered scripts
+	 * @return array reference to array of registered scripts
 	 */
 	public function getJsPlain ( )
 	{
@@ -279,23 +298,39 @@ class _app_registry
 	}
 
 	/**
+	 * Returns CSS fragments to be planted into <head> element.
+	 *
+	 * @return array reference to array of registered plain CSS defnitions
+	 */
+	public function getCssPlain ( )
+	{
+		$code = NULL;
+
+		if ( is_array( $this->cssPlain ) )
+			foreach ( $this->cssPlain as $piece )
+				$code .= $piece;
+
+		return $code;
+	}
+
+	/**
 	 * Returns CSS links.
 	 *
-	 * @return <array> reference to array of registered stylesheets
+	 * @return array reference to array of registered stylesheets
 	 */
 	public function getCss ( ) { return $this->cssLinks; }
 
 	/**
 	 * Returns <body> required children templates.
 	 *
-	 * @return <array> reference to array of registered template paths
+	 * @return array reference to array of registered template paths
 	 */
 	public function getBodyChildren ( ) { return $this->bodyChildren; }
 
 	/**
 	 * Composes and returns <body> onLoad event code to be executed.
 	 *
-	 * @return <string> Javascript code
+	 * @return string Javascript code
 	 */
 	public function getOnLoad ( )
 	{
@@ -311,7 +346,7 @@ class _app_registry
 	/**
 	 * Populates Smarty template engine with objects required in render phase.
 	 *
-	 * @param <Smarty> $smarty Smarty class instance
+	 * @param Smarty $smarty Smarty class instance
 	 * @todo change deployment to static value when deploying
 	 */
 	public function render (  )
@@ -324,7 +359,7 @@ class _app_registry
 	 * Broadcasts event to all registered applications. Typical event is update
 	 * of settings so application will reload their resources.
 	 *
-	 * @param <int> $event
+	 * @param int $event
 	 */
 	public function signal ( $event )
 	{
@@ -336,7 +371,7 @@ class _app_registry
 	/**
 	 * Returns reference to first application and resets internal pointer.
 	 *
-	 * @return <mixed> reference to app instance or NULL;
+	 * @return mixed reference to app instance or NULL;
 	 */
 	public function getFirst ( )
 	{
@@ -350,7 +385,7 @@ class _app_registry
 	 * Moves internal pointer one step forward and returns reference to
 	 * application on actual position.
 	 *
-	 * @return <mixed> reference to app instance or NULL;
+	 * @return mixed reference to app instance or NULL;
 	 */
 	public function getNext ( )
 	{
@@ -363,14 +398,9 @@ class _app_registry
 	/**
 	 * Returns reference to app by its id.
 	 *
-	 * @param <string> $id application identifier
+	 * @param string $id application identifier
 	 */
-	public function getById( $id )
-	{
-		/*debug_print_backtrace();
-		echo "\n\n";*/
-		return $this->apps[$id];
-	}
+	public function getById( $id ) { return $this->apps[$id]; }
 }
 
 ?>
