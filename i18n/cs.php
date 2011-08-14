8cd2e90e3c0b08811b7542506c619ca56c55de83
Ján Staník <morb@indra.local>
2011-08-14 21:52:51 +0100

    Fixing typos in Slovak and Czech localizations. Code cleanup.

--- i18n/cs.php
+++ i18n/cs.php
@@ -12,27 +12,27 @@
 /**
  * Search solution.
  */
-$__chassis_msg['srchBtSearch']		= 'Hledej';
-$__chassis_msg['srchBtShowAll']		= 'Zobraz vše';
+$__chassis_msg['srchBtSearch']			= 'Hledej';
+$__chassis_msg['srchBtShowAll']			= 'Zobraz vše';
 
 /**
  * List subpackage.
  */
 $__chassis_msg['listOf']				= 'z';
 $__chassis_msg['listTo']				= '-';
-$__chassis_msg['listItems']			= 'Záznamy';
-$__chassis_msg['listPage']			= 'Strana';
-$__chassis_msg['listEmptyOptions']	= 'Mužete';
-$__chassis_msg['listEmptyLastOpt']	= 'nebo';
+$__chassis_msg['listItems']				= 'Záznamy';
+$__chassis_msg['listPage']				= 'Strana';
+$__chassis_msg['listEmptyOptions']		= 'Mužete';
+$__chassis_msg['listEmptyLastOpt']		= 'nebo';
 
 /**
  * Lists Batch Processing (BP) form.
  */
-$__chassis_msg['bpAll']				= 'Označ vše';
+$__chassis_msg['bpAll']					= 'Označ vše';
 $__chassis_msg['bpNone']				= 'Odznač vše';
-$__chassis_msg['bpYes']				= 'Ano';
-$__chassis_msg['bpNo']				= 'Ne';
-$__chassis_msg['bpWarning']			= 'Varování';
+$__chassis_msg['bpYes']					= 'Ano';
+$__chassis_msg['bpNo']					= 'Ne';
+$__chassis_msg['bpWarning']				= 'Varování';
 
 $__chassis_msg['formBtBack']			= 'Zpět';
 $__chassis_msg['formBtSave']			= 'Uložit';
@@ -40,50 +40,50 @@ $__chassis_msg['formBtSave']			= 'Uložit';
 /**
  * CDES.
  */
-$__chassis_msg['cdesCreateContext']	= 'Vytvořit nálepku';
+$__chassis_msg['cdesCreateContext']		= 'Vytvořit nálepku';
 $__chassis_msg['cdesEditContext']		= 'Upravit nálepku';
 $__chassis_msg['cdesCaption']			= $__chassis_msg['cdesCreateContext'];
 $__chassis_msg['cdesPreview']			= 'Náhled';
-$__chassis_msg['cdesScheme']			= 'Farebná schéma';
+$__chassis_msg['cdesScheme']			= 'Barevná schéma';
 $__chassis_msg['cdesDisplay']			= 'Zobrazit text';
-$__chassis_msg['cdesDesc']			= 'Popis';
+$__chassis_msg['cdesDesc']				= 'Popis';
 $__chassis_msg['cdesContext']			= 'Nálepka';
 $__chassis_msg['cdesRemove']			= 'Odstranit';
-$__chassis_msg['cdesQuestion']		= 'Skutočne chcete zmazat obsah? <b>%s</b>? Tato operace je nevratná.';
-$__chassis_msg['cdesEmpty']			= 'Nejsou vytvořené žádne nálepky.';
+$__chassis_msg['cdesQuestion']			= 'Skutečně chcete zmazat nálepku <b>%s</b>? Tato operace je nevratná.';
+$__chassis_msg['cdesEmpty']				= 'Nejsou vytvořeny žádné nálepky.';
 $__chassis_msg['cdesNoMatch']			= 'Vyhledávání bylo neúspěšné.';
 $__chassis_msg['cdesOCreate']			= 'Vytvořit nálepku';
-$__chassis_msg['cdesOShowAll']		= 'Zobrazit všechny nálepky';
+$__chassis_msg['cdesOShowAll']			= 'Zobrazit všechny nálepky';
 $__chassis_msg['cdesOSearch']			= 'Změnit výraz a hledat znovu';
 $__chassis_msg['cdesNoCtxs']			= 'Žádny obsah není dostupný';
 
 /**
  * Contexts + CDES.
  */
-$__chassis_msg['ctx']['dar']			= 'Darth Vader';		// black scheme
+$__chassis_msg['ctx']['dar']			= 'Darth Vader';	// black scheme
 $__chassis_msg['ctx']['des']			= 'Desire';			// red scheme
 $__chassis_msg['ctx']['blu']			= 'Blue Wave';		// blue scheme
 $__chassis_msg['ctx']['wee']			= 'Surak';			// green scheme
 $__chassis_msg['ctx']['roq']			= 'Roquefort';		// magenta scheme
 $__chassis_msg['ctx']['flw']			= 'Sunflower';		// orange scheme
 $__chassis_msg['ctx']['sky']			= 'Skynet';			// blue scheme
-$__chassis_msg['ctx']['cle']			= 'Vogon Poetry';		// darker gray scheme
-$__chassis_msg['ctx']['sun']			= 'Sunshine';			// yellow scheme
+$__chassis_msg['ctx']['cle']			= 'Vogon Poetry';	// darker gray scheme
+$__chassis_msg['ctx']['sun']			= 'Sunshine';		// yellow scheme
 $__chassis_msg['ctx']['sil']			= 'Argentina';		// gray scheme
 $__chassis_msg['ctx']['sio']			= 'Zion Gates';		// white scheme with edges
 
 /**
  * CDES status messages.
  */
-$__chassis_msg['cdes']['preparing']	= 'Připravujem formulář...';
-$__chassis_msg['cdes']['prepared']	= 'Připravený';
+$__chassis_msg['cdes']['preparing']		= 'Připravujem formulář...';
+$__chassis_msg['cdes']['prepared']		= 'Připravený';
 $__chassis_msg['cdes']['saving']		= 'Ukládám nálepku...';
-$__chassis_msg['cdes']['saved']		= 'Uložené';
-$__chassis_msg['cdes']['executing']	= 'Vykonávám...';
-$__chassis_msg['cdes']['executed']	= 'Vykonané';
-$__chassis_msg['cdes']['e_format']	= 'Chyba: nesprávný obsah ' . $__chassis_msg['cdesDisplay'] . '!';
-$__chassis_msg['cdes']['e_exists']	= 'Chyba: obsah už existuje!';
-$__chassis_msg['cdes']['e_unknown']	= 'Chyba: chyba neznámá! Kontaktujte správcov.';
+$__chassis_msg['cdes']['saved']			= 'Uložené';
+$__chassis_msg['cdes']['executing']		= 'Vykonávám...';
+$__chassis_msg['cdes']['executed']		= 'Vykonané';
+$__chassis_msg['cdes']['e_format']		= 'Chyba: nesprávný obsah ' . $__chassis_msg['cdesDisplay'] . '!';
+$__chassis_msg['cdes']['e_exists']		= 'Chyba: obsah již existuje!';
+$__chassis_msg['cdes']['e_unknown']		= 'Chyba: neznámá chyba! Kontaktujte správce.';
 
 /**
  * Search solution status messages.
@@ -91,12 +91,12 @@ $__chassis_msg['cdes']['e_unknown']	= 'Chyba: chyba neznámá! Kontaktujte sprá
  * @todo rethink about more messages to indicate error conditions (see CDES part)
  */
 $__chassis_msg['srch']['loading']		= 'Načítávám...';
-$__chassis_msg['srch']['loaded']		= 'Načítané';
-$__chassis_msg['srch']['resizing']	= 'Měním velikost...';
-$__chassis_msg['srch']['resized']		= 'Velikost změnená';
-$__chassis_msg['srch']['executing']	= $__chassis_msg['cdes']['executing'];
-$__chassis_msg['srch']['executed']	= $__chassis_msg['cdes']['executed'];
-$__chassis_msg['srch']['e_unknown']	= $__chassis_msg['cdes']['e_unknown'];
+$__chassis_msg['srch']['loaded']		= 'Načteno';
+$__chassis_msg['srch']['resizing']		= 'Měním velikost...';
+$__chassis_msg['srch']['resized']		= 'Velikost změnena';
+$__chassis_msg['srch']['executing']		= $__chassis_msg['cdes']['executing'];
+$__chassis_msg['srch']['executed']		= $__chassis_msg['cdes']['executed'];
+$__chassis_msg['srch']['e_unknown']		= $__chassis_msg['cdes']['e_unknown'];
 
 /**
  * SkyDome messages.
--- i18n/sk.php
+++ i18n/sk.php
@@ -12,27 +12,27 @@
 /**
  * Search solution.
  */
-$__chassis_msg['srchBtSearch']		= 'Hľadaj';
-$__chassis_msg['srchBtShowAll']		= 'Zobraz všetko';
+$__chassis_msg['srchBtSearch']			= 'Hľadaj';
+$__chassis_msg['srchBtShowAll']			= 'Zobraz všetko';
 
 /**
  * List subpackage.
  */
 $__chassis_msg['listOf']				= 'z';
 $__chassis_msg['listTo']				= '-';
-$__chassis_msg['listItems']			= 'Záznamy';
-$__chassis_msg['listPage']			= 'Strana';
-$__chassis_msg['listEmptyOptions']	= 'Môžete';
-$__chassis_msg['listEmptyLastOpt']	= 'alebo';
+$__chassis_msg['listItems']				= 'Záznamy';
+$__chassis_msg['listPage']				= 'Strana';
+$__chassis_msg['listEmptyOptions']		= 'Môžete';
+$__chassis_msg['listEmptyLastOpt']		= 'alebo';
 
 /**
  * Lists Batch Processing (BP) form.
  */
-$__chassis_msg['bpAll']				= 'Označ všetko';
+$__chassis_msg['bpAll']					= 'Označ všetko';
 $__chassis_msg['bpNone']				= 'Odznač všetko';
-$__chassis_msg['bpYes']				= 'Áno';
-$__chassis_msg['bpNo']				= 'Nie';
-$__chassis_msg['bpWarning']			= 'Varovanie';
+$__chassis_msg['bpYes']					= 'Áno';
+$__chassis_msg['bpNo']					= 'Nie';
+$__chassis_msg['bpWarning']				= 'Varovanie';
 
 $__chassis_msg['formBtBack']			= 'Späť';
 $__chassis_msg['formBtSave']			= 'Uložiť';
@@ -40,20 +40,20 @@ $__chassis_msg['formBtSave']			= 'Uložiť';
 /**
  * CDES.
  */
-$__chassis_msg['cdesCreateContext']	= 'Vytvoriť nálepku';
+$__chassis_msg['cdesCreateContext']		= 'Vytvoriť nálepku';
 $__chassis_msg['cdesEditContext']		= 'Upraviť nálepku';
 $__chassis_msg['cdesCaption']			= $__chassis_msg['cdesCreateContext'];
 $__chassis_msg['cdesPreview']			= 'Náhľad';
 $__chassis_msg['cdesScheme']			= 'Farebná schéma';
 $__chassis_msg['cdesDisplay']			= 'Zobraziť text';
-$__chassis_msg['cdesDesc']			= 'Popis';
+$__chassis_msg['cdesDesc']				= 'Popis';
 $__chassis_msg['cdesContext']			= 'Nálepka';
 $__chassis_msg['cdesRemove']			= 'Odstrániť';
-$__chassis_msg['cdesQuestion']		= 'Skutočne chcete zmazať obsah <b>%s</b>? Táto operácia je nevratná.';
-$__chassis_msg['cdesEmpty']			= 'Nie sú vytvorené žiadne nálepky.';
+$__chassis_msg['cdesQuestion']			= 'Skutočne chcete zmazať nálepku <b>%s</b>? Táto operácia je nevratná.';
+$__chassis_msg['cdesEmpty']				= 'Nie sú vytvorené žiadne nálepky.';
 $__chassis_msg['cdesNoMatch']			= 'Vyhľadávanie bolo neúspešné.';
 $__chassis_msg['cdesOCreate']			= 'Vytvoriť nálepku';
-$__chassis_msg['cdesOShowAll']		= 'Zobraziť všetky nálepky';
+$__chassis_msg['cdesOShowAll']			= 'Zobraziť všetky nálepky';
 $__chassis_msg['cdesOSearch']			= 'Zmeniť výraz a hľadať znovu';
 $__chassis_msg['cdesNoCtxs']			= 'Žiadny obsah nie je dostupný';
 
@@ -75,15 +75,15 @@ $__chassis_msg['ctx']['sio']			= 'Zion Gates';		// white scheme with edges
 /**
  * CDES status messages.
  */
-$__chassis_msg['cdes']['preparing']	= 'Pripravujem formulár...';
-$__chassis_msg['cdes']['prepared']	= 'Pripravený';
+$__chassis_msg['cdes']['preparing']		= 'Pripravujem formulár...';
+$__chassis_msg['cdes']['prepared']		= 'Pripravený';
 $__chassis_msg['cdes']['saving']		= 'Ukladám nálepku...';
-$__chassis_msg['cdes']['saved']		= 'Uložené';
-$__chassis_msg['cdes']['executing']	= 'Vykonávam...';
-$__chassis_msg['cdes']['executed']	= 'Vykonané';
-$__chassis_msg['cdes']['e_format']	= 'Chyba: nesprávny obsah ' . $__chassis_msg['cdesDisplay'] . '!';
-$__chassis_msg['cdes']['e_exists']	= 'Chyba: obsah už existuje!';
-$__chassis_msg['cdes']['e_unknown']	= 'Chyba: chyba neznáma! Kontaktujte správcov.';
+$__chassis_msg['cdes']['saved']			= 'Uložené';
+$__chassis_msg['cdes']['executing']		= 'Vykonávam...';
+$__chassis_msg['cdes']['executed']		= 'Vykonané';
+$__chassis_msg['cdes']['e_format']		= 'Chyba: nesprávny obsah ' . $__chassis_msg['cdesDisplay'] . '!';
+$__chassis_msg['cdes']['e_exists']		= 'Chyba: obsah už existuje!';
+$__chassis_msg['cdes']['e_unknown']		= 'Chyba: neznáma chyba! Kontaktujte správcov.';
 
 /**
  * Search solution status messages.
@@ -92,11 +92,11 @@ $__chassis_msg['cdes']['e_unknown']	= 'Chyba: chyba neznáma! Kontaktujte správ
  */
 $__chassis_msg['srch']['loading']		= 'Načítavam...';
 $__chassis_msg['srch']['loaded']		= 'Načítané';
-$__chassis_msg['srch']['resizing']	= 'Mením veľkosť...';
+$__chassis_msg['srch']['resizing']		= 'Mením veľkosť...';
 $__chassis_msg['srch']['resized']		= 'Veľkosť zmenená';
-$__chassis_msg['srch']['executing']	= $__chassis_msg['cdes']['executing'];
-$__chassis_msg['srch']['executed']	= $__chassis_msg['cdes']['executed'];
-$__chassis_msg['srch']['e_unknown']	= $__chassis_msg['cdes']['e_unknown'];
+$__chassis_msg['srch']['executing']		= $__chassis_msg['cdes']['executing'];
+$__chassis_msg['srch']['executed']		= $__chassis_msg['cdes']['executed'];
+$__chassis_msg['srch']['e_unknown']		= $__chassis_msg['cdes']['e_unknown'];
 
 /**
  * SkyDome messages.
