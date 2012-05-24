d2f3faa45a71e88ad05d034a1a08f62acf01a66e
Jan Stanik <jan.stanik@openet.com>
2012-05-24 22:05:07 +0100

    Improving Ajax adapter quality of code and opening it to decorators.

--- inc/js/_ajax_req_ad.js
+++ inc/js/_ajax_req_ad.js
@@ -8,6 +8,9 @@
  * @requires prototype.js
  */
 
+_ajax_req_ad.prototype = new Object( );
+_ajax_req_ad.prototype.constructor = _ajax_req_ad;
+
 /**
  * Ajax request adaptor. This serves as active interface to underlying routines.
  * Purpose is to make use of Ajax requests simpler within the framework and user
@@ -15,17 +18,12 @@
  * now it uses Prototype framework as backend. For callbacks array names from
  * Prototype framework callbacks are used (onCreate, onFailure, onSuccess, ...).
  * 
- * @param bool async sets mode of request (false=sync/blocking, true=async/non-blocking)
- * @param string url URL of Ajax server
- * @param array base associative array of base request parameters (passed from server side)
+ * @param async sets mode of request (false=sync/blocking, true=async/non-blocking)
+ * @param url URL of Ajax server
+ * @param base associative array of base request parameters (passed from server side)
  */
 function _ajax_req_ad ( async, url, base )
 {
-	/**
-	 * Copy scope.
-	 */
-	var me = this;
-	
 	/**
 	 * Request synchronization. True means non-blocking mode.
 	 */
@@ -40,72 +38,70 @@ function _ajax_req_ad ( async, url, base )
 	 * Common set of Ajax request parameters.
 	 */
 	this.base = base;
-	
-	/**
-	 * Prepares Ajax request data.
-	 * 
-	 * @param array extra associative array of extra request parameters (composed by client side/caller)
-	 * @param array cbs associative array of callbacks
-	 * @param array data associative array of additional POST method data parameter (is null in most of cases)
-	 * @param bool async method used for the request
-	 */
-	this.prepare = function ( extra, cbs, data, async )
-	{
-		var params = '';
-		
-		for ( var key in this.base )
-			params += '&' + key + '=' + this.base[key];
-		
-		for ( var key in extra )
-			params += '&' + key + '=' + extra[key];
-		
-		var full = cbs;		
-			full['asynchronous']	= async;
-			full['method']			= 'post';
-			full['parameters']		= params;
-			
-			/**
-			 * Extra POST data.
-			 */
-			if ( data != null )
-			{
-				post_body = params;
-				for ( var key in data )
-					post_body += '&' + key + '=' + data[key];
-				full['postBody'] = post_body;
-			}
-			
-		return full;
-	};
+}
 
+/**
+ * Prepares Ajax request data.
+ * 
+ * @param extra associative array of extra request parameters (composed by client side/caller)
+ * @param cbs associative array of callbacks
+ * @param data associative array of additional POST method data parameter (is null in most of cases)
+ * @param async method used for the request
+ */
+_ajax_req_ad.prototype.prepare = function ( extra, cbs, data, async )
+{
+	var params = '';
+		
+	for ( var key in this.base )
+		params += '&' + key + '=' + this.base[key];
+		
+	for ( var key in extra )
+		params += '&' + key + '=' + extra[key];
+		
+	var full = cbs;		
+		full['asynchronous']	= async;
+		full['method']			= 'post';
+		full['parameters']		= params;
+			
 	/**
-	 * Send-and-Receive data operation. This sends data to Ajax server URL and
-	 * performs callbacks for given Ajax event.
-	 * 
-	 * @param array extra associative array of extra request parameters (composed by client side/caller)
-	 * @param array cbs associative array of callbacks
-	 * @param array data associative array of additional POST method data parameter (is null in most of cases)
-	 * @param bool async if not null, explicitly changing default blocking behaviour
+	 * Extra POST data.
 	 */
-	this.send = function ( extra, cbs, data, async )
+	if ( data != null )
 	{
-		var req = new Ajax.Request(	me.url, me.prepare( extra, cbs, data, ( ( async !== null ) ? async : me.async ) ) );
-		return req;
-	};
+		post_body = params;
+		for ( var key in data )
+			post_body += '&' + key + '=' + data[key];
+		full['postBody'] = post_body;
+	}
 	
-	/**
-	 * Send-and-Show operation. This sends data to Ajax server URL, renders
-	 * response HTML into target element and performs callbacks.
-	 * 
-	 * @param array extra associative array of extra request parameters (composed by client side/caller)
-	 * @param array cbs associative array of callbacks
-	 * @param string target_id HTML ID of target container for the response content
-	 * @param array data associative array of additional POST method data parameter (is null in most of cases)
-	 * @param bool async if not null, explicitly changing default blocking behaviour
-	 */
-	this.update = function ( extra, cbs, target_id, data, async )
-	{
-		var req = new Ajax.Updater( target_id, me.url, me.prepare( extra, cbs, data, ( ( async !== null ) ? async : me.async ) ) );
-		return req;
-	};
+	return full;
+}
+	
+/**
+ * Send-and-Receive data operation. This sends data to Ajax server URL and
+ * performs callbacks for given Ajax event.
+ * 
+ * @param extra associative array of extra request parameters (composed by client side/caller)
+ * @param cbs associative array of callbacks
+ * @param data associative array of additional POST method data parameter (is null in most of cases)
+ * @param async if not null, explicitly changing default blocking behaviour
+ */
+_ajax_req_ad.prototype.send = function ( extra, cbs, data, async )
+{
+	return new Ajax.Request( this.url, this.prepare( extra, cbs, data, ( ( async !== null ) ? async : this.async ) ) );
+}
+
+/**
+ * Send-and-Show operation. This sends data to Ajax server URL, renders
+ * response HTML into target element and performs callbacks.
+ * 
+ * @param extra associative array of extra request parameters (composed by client side/caller)
+ * @param cbs associative array of callbacks
+ * @param target_id HTML ID of target container for the response content
+ * @param data associative array of additional POST method data parameter (is null in most of cases)
+ * @param async if not null, explicitly changing default blocking behaviour
+ */
+_ajax_req_ad.prototype.update = function ( extra, cbs, target_id, data, async )
+{
+	return new Ajax.Updater( target_id, this.url, this.prepare( extra, cbs, data, ( ( async !== null ) ? async : this.async ) ) );
 }
